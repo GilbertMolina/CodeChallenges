6a135b7e6542315c98f1ae5f976b5d9a1e0e3a74
Gilbert Molina <gmolinac@outlook.com>
2023-06-21 14:37:27 -0600

    Update LongestWord.js

--- codechallenges/LongestWord.js
+++ codechallenges/LongestWord.js
@@ -6,24 +6,24 @@ Longest Word
 Resolution:
 */
 function LongestWord(sen) {
-    let wordsLengthArray = []
+  let wordsLengthArray = [];
 
-    sen.split(' ').forEach(x => wordsLengthArray.push(lettersOnly(x)))
+  sen.split(" ").forEach((x) => wordsLengthArray.push(lettersOnly(x)));
 
-    let maxIndex = wordsLengthArray.indexOf(Math.max.apply(Math, wordsLengthArray))
+  let maxIndex = wordsLengthArray.indexOf(Math.max.apply(Math, wordsLengthArray));
 
-    return sen.split(' ')[maxIndex]
+  return sen.split(" ")[maxIndex];
 }
 
 function lettersOnly(str) {
-    str = str.toLowerCase()
-    let wordLength = 0
+  str = str.toLowerCase();
+  let wordLength = 0;
 
-    for (let i = 0; i < str.length; i++) {
-        wordLength += (str[i].charCodeAt() - 96) > 0 && (str[i].charCodeAt() - 96) <= 26 ? 1 : 0
-    }
+  for (let i = 0; i < str.length; i++) {
+    wordLength += str[i].charCodeAt() - 96 > 0 && str[i].charCodeAt() - 96 <= 26 ? 1 : 0;
+  }
 
-    return wordLength
+  return wordLength;
 }
 
-LongestWord('I love dogs')
+console.log(LongestWord("I love dogs"));
